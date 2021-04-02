7729a0cbcaa03d73a64d37ca17c0a5f751bca87c
John Fuquene <jfuquene@email.wm.edu>
2021-04-02 17:40:11 -0400

    second_time

--- src/Comment.js
+++ src/Comment.js
@@ -5,6 +5,7 @@ export default class Comment extends React.Component {
     render() {
         return (
             <div className="comment">
+                //receives information from blogpost
                  {this.props.commentText}
             </div>    
         )
